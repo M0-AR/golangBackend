bb5e3ceff43789fb43a270541823b849b8b1b453
m0-ar <s176492@student.dtu.dk>
2021-10-25 15:33:02 +0200

    Add AppError

--- src/dao/service_dao.go
+++ src/dao/service_dao.go
@@ -2,30 +2,36 @@ package dao
 
 import (
 	"fmt"
+	"github.com/B1gDaddyKane/golangBackend/src/errors"
 	"github.com/B1gDaddyKane/golangBackend/src/model"
+	"net/http"
 	"time"
 )
 
 // Fake Data
 var (
-	services = []model.Service{
+	services = []*model.Service{
 		{ID: 1, Title: "Wash Machine", Price: 450, ImageUrl: "", IsAvailable: true, ServiceStartDate: time.Time{}},
 		{ID: 2, Title: "Wash Machine02", Price: 450, ImageUrl: "", IsAvailable: true, ServiceStartDate: time.Time{}},
 	}
 )
 
-func GetServices() ([]model.Service, error) {
+func GetServices() ([]*model.Service, *errors.AppError) {
 	if services != nil {
 		return services, nil
 	}
 
-	return nil, fmt.Errorf("services are empty")
+	return nil, &errors.AppError{
+		Message:    fmt.Sprintf("Services were not found"),
+		StatusCode: http.StatusNotFound,
+		Status:     "not_found",
+	}
 }
 
 func GetServicesById(serviceId int) (*model.Service, error) {
 	for _, item := range services {
 		if item.ID == serviceId {
-			return &item, nil
+			return item, nil
 		}
 	}
 	return nil, fmt.Errorf("service id not found")
