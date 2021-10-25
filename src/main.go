bc5b2ddeb29c89846b695ec1da275eb7abbe2d9e
m0-ar <s176492@student.dtu.dk>
2021-10-25 16:57:27 +0200

    GetServiceById is being tested by postman -> OK

--- src/main.go
+++ src/main.go
@@ -27,7 +27,7 @@ func handleRequest() {
 	// Dashboard -> Service
 	// Todo: Can't not differentiate between these to GET method
 	//router.HandleFunc("/dashboard/services", controllers.GetServices).Methods("GET")
-	router.HandleFunc("/dashboard/services", controllers.GetServiceById).Methods("GET")
+	router.HandleFunc("/dashboard/services", controllers.GetServiceById).Methods("GET") // Test done by postman
 
 	fmt.Print("Server start at PORT 10000\n")
 	log.Fatal(http.ListenAndServe(":10000", router))
