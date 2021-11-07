634063015433ee2964e6f56b14962d4c5c928438
m0-ar <s176492@student.dtu.dk>
2021-11-07 12:32:34 +0100

    Create router.go

---
+++ src/delivery/http/router.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"github.com/B1gDaddyKane/golangBackend/src/delivery/controllers"
+	"github.com/B1gDaddyKane/golangBackend/src/interfaces"
+	"github.com/labstack/echo/v4"
+)
+
+func NewRouter(e *echo.Echo, serviceUseCase interfaces.ServiceUseCaseI) {
+
+	// Dashboard -> Services
+	serviceCtrl := controllers.NewServiceController(e, serviceUseCase)
+
+	r := e.Group("/api/v1/go-mongo")
+	r.GET("/dashboard/list", serviceCtrl.GetServices)
+	r.POST("/dashboard/add", serviceCtrl.CreateService)
+
+}
