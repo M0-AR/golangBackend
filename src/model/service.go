a9752b4b46b76a670a0436671b4b6fab48f1ce34
m0-ar <s176492@student.dtu.dk>
2021-11-07 11:26:27 +0100

    Update service.go model

--- src/model/service.go
+++ src/model/service.go
@@ -4,12 +4,18 @@ import (
 	"time"
 )
 
-// Service represents data about a record Service.
-type Service struct {
-	ID               int64     `json:"id"`
-	Title            string    `json:"title"`
-	Price            float64   `json:"price"`
-	ImageUrl         string    `json:"image_url"`
-	IsAvailable      bool      `json:"is_available"`
-	ServiceStartDate time.Time `json:"service_start_date"`
-}
+type (
+	GetServicesResponse struct {
+		Services []Service `json:"services"`
+	}
+
+	// Service represents data about a record Service.
+	Service struct {
+		ID               int64     `json:"id" bson:"_id"`
+		Title            string    `json:"title" bson:"title"`
+		Price            float64   `json:"price" bson:"price"`
+		ImageUrl         string    `json:"image_url" bson:"image_url"`
+		IsAvailable      bool      `json:"is_available" bson:"is_available"`
+		ServiceStartDate time.Time `json:"service_start_date" bson:"service_start_date"`
+	}
+)
