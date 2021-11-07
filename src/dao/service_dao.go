44e077760d726ae7bc305b25095badffe28c8dc0
m0-ar <s176492@student.dtu.dk>
2021-11-07 11:07:10 +0100

    Update service_dao.go -> mongodb data instead of memory data

--- src/dao/service_dao.go
+++ src/dao/service_dao.go
@@ -1,42 +1,85 @@
 package dao
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"github.com/B1gDaddyKane/golangBackend/src/errors"
+	"github.com/B1gDaddyKane/golangBackend/src/interfaces"
 	"github.com/B1gDaddyKane/golangBackend/src/model"
-	"net/http"
-	"time"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+	//"time"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// Fake Data
-var (
-	services = []*model.Service{
-		{ID: 1, Title: "Wash Machine", Price: 450, ImageUrl: "", IsAvailable: true, ServiceStartDate: time.Time{}},
-		{ID: 2, Title: "Wash Machine02", Price: 450, ImageUrl: "", IsAvailable: true, ServiceStartDate: time.Time{}},
-	}
-)
+type ServiceDAO struct {
+	mongoDB *mongo.Database
+}
 
-func GetServices() ([]*model.Service, *errors.AppError) {
-	if services != nil {
-		return services, nil
-	}
-
-	return nil, &errors.AppError{
-		Message:    fmt.Sprintf("Services were not found"),
-		StatusCode: http.StatusNotFound,
-		Status:     "not_found",
+func NewServiceDAO(mongo *mongo.Database) interfaces.ServiceDAOI {
+	return &ServiceDAO{
+		mongoDB: mongo,
 	}
 }
 
-func GetServiceById(serviceId int64) (*model.Service, *errors.AppError) {
-	for _, item := range services {
-		if item.ID == serviceId {
-			return item, nil
+func (sdao ServiceDAO) GetServices(ctx context.Context) (resp model.GetServicesResponse, err error) {
+
+	query, err := sdao.mongoDB.Collection("services").Find(ctx, bson.D{})
+	if err != nil {
+		log.Println("error", err)
+		return model.GetServicesResponse{}, err
+	}
+	defer query.Close(ctx)
+
+	listServices := make([]model.Service, 0)
+	for query.Next(ctx) {
+		var row model.Service
+		err := query.Decode(&row)
+		if err != nil {
+			log.Println("error")
 		}
+		listServices = append(listServices, row)
 	}
-	return nil, &errors.AppError{
-		Message:    fmt.Sprintf("Service %v was not found", serviceId),
-		StatusCode: http.StatusNotFound,
-		Status:     "not_found",
+
+	resp = model.GetServicesResponse{Services: listServices}
+
+	return resp, err
+
+}
+
+func (sdao ServiceDAO) CreateService(ctx context.Context, req model.Service) error {
+
+	dataReq := bson.M{
+		"title": req.Title,
 	}
+
+	query, err := sdao.mongoDB.Collection("services").InsertOne(ctx, dataReq)
+	if err != nil {
+		log.Println("error")
+	}
+
+	if oid, ok := query.InsertedID.(primitive.ObjectID); ok {
+		serviceID := oid.Hex()
+		updateServiceID := bson.M{"_id": oid}
+		objectID := bson.M{"$set": bson.M{"_id": serviceID}}
+		_, err = sdao.mongoDB.Collection("services").UpdateOne(ctx, updateServiceID, objectID)
+		if err != nil {
+			log.Println("error")
+		}
+	} else {
+		err = errors.New(fmt.Sprint("can't get inserted ID", err))
+		log.Println("error")
+	}
+
+	return err
+
+}
+
+func (sdao ServiceDAO) UpdateService(ctx context.Context, req model.Service) error {
+	panic("implement me")
+}
+
+func (sdao ServiceDAO) DeleteService(ctx context.Context, req model.Service) error {
+	panic("implement me")
 }
