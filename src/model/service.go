b2b2696f0b2102119e9a5a04e6780094bba95e95
m0-ar <s176492@student.dtu.dk>
2021-11-07 12:35:26 +0100

    Update service.go

--- src/model/service.go
+++ src/model/service.go
@@ -9,13 +9,21 @@ type (
 		Services []Service `json:"services"`
 	}
 
-	// Service represents data about a record Service.
 	Service struct {
-		ID               int64     `json:"id" bson:"_id"`
+		ID               int64     `json:"id,omitempty" bson:"_id"`
 		Title            string    `json:"title" bson:"title"`
 		Price            float64   `json:"price" bson:"price"`
 		ImageUrl         string    `json:"image_url" bson:"image_url"`
 		IsAvailable      bool      `json:"is_available" bson:"is_available"`
 		ServiceStartDate time.Time `json:"service_start_date" bson:"service_start_date"`
+	}
+
+	ServiceRequest struct {
+		ID               int64     `json:"id,omitempty" bson:"_id"`
+		Title            string    `json:"title,omitempty" bson:"title"`
+		Price            float64   `json:"price,omitempty" bson:"price"`
+		ImageUrl         string    `json:"image_url,omitempty" bson:"image_url"`
+		IsAvailable      bool      `json:"is_available,omitempty" bson:"is_available"`
+		ServiceStartDate time.Time `json:"service_start_date,omitempty" bson:"service_start_date"`
 	}
 )
