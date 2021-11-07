3819dbf38ad1b22c4ea191c75e344520cf416c43
m0-ar <s176492@student.dtu.dk>
2021-11-07 10:08:30 +0100

    Create ServiceUseCaseI

---
+++ src/interfaces/ServiceUseCaseI.go
@@ -0,0 +1,13 @@
+package interfaces
+
+import (
+	"context"
+	"github.com/B1gDaddyKane/golangBackend/src/model"
+)
+
+type ServiceUseCaseI interface {
+	GetServices(ctx context.Context) (resp model.GetServicesResponse, err error)
+	CreateService(ctx context.Context, req model.Service) (resp bool, err error)
+	UpdateService(ctx context.Context, req model.Service) (resp bool, err error)
+	DeleteService(ctx context.Context, req model.Service) (resp bool, err error)
+}
