2b5d0b2503a213c8b719781d0b6be13e9832822e
m0-ar <s176492@student.dtu.dk>
2021-11-12 20:32:26 +0100

    ok: create from frontend to database service

--- src/model/service.go
+++ src/model/service.go
@@ -10,7 +10,7 @@ type (
 	Service struct {
 		ServiceID          string    `json:"service_id,omitempty" bson:"service_id"`
 		ServiceTitle       string    `json:"service_title" bson:"service_title"`
-		ServicePrice       float64   `json:"service_price" bson:"service_price"`
+		ServicePrice       string    `json:"service_price" bson:"service_price"`
 		ServiceImageUrl    string    `json:"service_image_url" bson:"service_image_url"`
 		ServiceIsAvailable bool      `json:"service_is_available" bson:"service_is_available"`
 		ServiceStartDate   time.Time `json:"service_start_date" bson:"service_start_date"`
@@ -19,7 +19,7 @@ type (
 	ServiceRequest struct {
 		ServiceID          string    `json:"service_id,omitempty" bson:"service_id"`
 		ServiceTitle       string    `json:"service_title,omitempty" bson:"service_title"`
-		ServicePrice       float64   `json:"service_price,omitempty" bson:"service_price"`
+		ServicePrice       string    `json:"service_price,omitempty" bson:"service_price"`
 		ServiceImageUrl    string    `json:"service_image_url,omitempty" bson:"service_image_url"`
 		ServiceIsAvailable bool      `json:"service_is_available,omitempty" bson:"service_is_available"`
 		ServiceStartDate   time.Time `json:"service_start_date,omitempty" bson:"service_start_date"`
